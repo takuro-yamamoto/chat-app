7cddc9edf169d9c3d73929f0c2c3aba1c4463b32
takuro-yamamoto <taku50lax@outlook.jp>
2020-12-20 16:59:42 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  has_many :messages
+  has_many :rooms, through: :room_users
 end
